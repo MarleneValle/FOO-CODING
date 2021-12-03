81a2231bb8ed9ac726f041a418873fddcfeb7f5f
Marlene Valle <marlenevalle@live.com.mx>
2021-12-03 22:14:56 +0100

    JavaScript

--- JavaScript/JavaScript_course/projects/firebase/src/index.js
+++ JavaScript/JavaScript_course/projects/firebase/src/index.js
@@ -3,11 +3,24 @@ import { initializeApp } from "firebase/app";
 import {
   getFirestore,
   collection,
-  getDocs,
+  onSnapshot,
   addDoc,
   deleteDoc,
   doc,
+  query,
+  where,
+  orderBy,
+  serverTimestamp,
+  getDoc,
+  updateDoc,
 } from "firebase/firestore";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -23,20 +36,28 @@ initializeApp(firebaseConfig);
 
 //init services
 const db = getFirestore();
+const auth = getAuth();
+
 // collection ref
 const colRef = collection(db, "books");
-// get collection data, which returns a promise
-getDocs(colRef)
-  .then((snapshot) => {
-    const books = [];
-    snapshot.docs.forEach((doc) => {
-      books.push({ ...doc.data(), id: doc.id });
-    });
-    console.log(books);
-  })
-  .catch((err) => {
-    console.log(err.message);
+
+//queries
+const q = query(
+  colRef,
+  // where("author", "==", "patrick rothfuss"),
+  //order by timestamp
+  orderBy("createdAt", "desc")
+);
+
+// real time collection data
+
+const unsubCol = onSnapshot(q, (snapshot) => {
+  let books = [];
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
   });
+  console.log(books);
+});
 
 //adding documents
 const addBookForm = document.querySelector(".add");
@@ -45,6 +66,8 @@ addBookForm.addEventListener("submit", (e) => {
   addDoc(colRef, {
     title: addBookForm.title.value,
     author: addBookForm.author.value,
+    //add timestamp
+    createdAt: serverTimestamp(),
   }).then(() => {
     addBookForm.reset();
   });
@@ -60,4 +83,91 @@ deleteBookForm.addEventListener("submit", (e) => {
   deleteDoc(docRef).then(() => {
     deleteBookForm.reset();
   });
+});
+
+//get a single doc
+
+const docRef = doc(db, "books", "RZFmA68P9umNTyloQYAF");
+
+// getDoc(docRef).then((doc) => {
+//   console.log(doc.data(), doc.id);
+// });
+
+//real time document data
+const unsubDoc = onSnapshot(docRef, (doc) => {
+  console.log(doc.data(), doc.id);
+});
+
+//updating a document
+const updateForm = document.querySelector(".update");
+updateForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const docRef = doc(db, "books", updateForm.id.value);
+
+  updateDoc(docRef, {
+    title: "The new AMor",
+  }).then(() => {
+    updateForm.reset();
+  });
+});
+
+//signing user up
+const signupForm = document.querySelector(".signup");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = signupForm.email.value;
+  const password = signupForm.password.value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      // console.log("user created:", cred.user);
+      signupForm.reset();
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+//logging in and out
+//LOGOUT
+const logoutButton = document.querySelector(".logout");
+logoutButton.addEventListener("click", () => {
+  signOut(auth)
+    .then(() => {
+      // console.log("the user signed out");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+//LOGIN
+const loginForm = document.querySelector(".login");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = loginForm.email.value;
+  const password = loginForm.password.value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log("user logged in:", cred.user);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+//subscription to auth changes
+const unsubAuth = onAuthStateChanged(auth, (user) => {
+  console.log("user status changed:", user);
+});
+
+//unsubscribing from changes (auth &db)
+const unsubButton = document.querySelector(".unsub");
+unsubButton.addEventListener("click", () => {
+  console.log("unsubscribing");
+  unsubCol();
+  unsubDoc();
+  unsubAuth();
 });
