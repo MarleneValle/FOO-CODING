59d34f7b1d4c5c660e2b6deef654a71f2f90b045
Marlene Valle <marlenevalle@live.com.mx>
2021-12-03 22:30:45 +0100

    Modern-JavaScript changed to a new repo

--- JavaScript/JavaScript_course/chapter_19_Workflow/Babel/src/index.js
+++
@@ -1,6 +0,0 @@
-import { addTitle, styleBody, contact } from "./dom";
-import whatever, { getPremUser } from "./data";
-
-const premUsers = getPremUser(whatever);
-console.log(whatever, premUsers);
-console.log("test");
